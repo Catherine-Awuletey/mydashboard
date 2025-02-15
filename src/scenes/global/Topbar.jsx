c64a6fee85c77c77e3a50be775d39ec90410afc4
Cattie <87996502+Catherine-Awuletey@users.noreply.github.com>
2025-02-15 18:15:56 +0000

    Initial commit

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,19 @@
+import { Box, IconButtion, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsModeOutlinedIcon from "@mui/icons-material/NotificationsModeOutlined";
+import SettingsModeOutlinedIcon from "@mui/icons-material/SettingsModeOutlined";
+import PersonModeOutlinedIcon from "@mui/icons-material/PersonModeOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
 const Topbar = () => {
-    return <div>Topbar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+    return (<Box display="flex" justifyContent="space-between" p={2}></Box>);
 }
 
 export default Topbar;
